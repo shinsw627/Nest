fccf697e9c970965bbe9413a93a65c90575f55ae
shinsw627 <shinsw627@naver.com>
2022-07-19 23:26:04 +0900

    add: auth 컨트롤러 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  loginUser(@Req() req: Request, @Body() Body) {}
+
+  @Get()
+  logoutUser() {}
 }
